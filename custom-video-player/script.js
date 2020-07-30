2972ac9e35c7197e01fff9b4853ef086eca2500e
Lucy Chang <lucalucy0220@gmail.com>
2020-07-30 10:15:29 +1000

    custom video player done

---
+++ custom-video-player/script.js
@@ -0,0 +1,64 @@
+const video = document.getElementById("video");
+const play = document.getElementById("play");
+const stop = document.getElementById("stop");
+const progress = document.getElementById("progress");
+const timestamp = document.getElementById("timestamp");
+
+// toggle  video's status when function is called
+function toggleVideoStatus() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+// change the play icon according to video's status
+function updatePlayIcon() {
+    if (video.paused){
+        play.innerHTML="<i class="fa fa-play fa-2x"></i>"
+    }else{
+        play.innerHTML="<i class="fa fa-pause fa-2x"></i>"
+    }
+}
+
+// update the progress bar and timestamp as the video is playing
+function updateProgress() {
+    // update timestamp
+    let mins = Math.floor(video.currentTime/60);
+    if (mins<10){
+        mins = "0"+String(mins);
+    }
+    let secs = Math.floor(video.currentTime%60);
+    if (secs<10){
+        secs = "0"+String(secs);
+    }
+    timestamp.innerHTML=`${mins}:${secs}`;
+
+    // update progress bar
+    progress.value = (video.currentTime/video.duration)/100;
+}
+
+// update video when user manually adjust progess bar
+function setVideoProgress() {
+    video.currentTime=(+progress.value*video.duration)/100;
+}
+
+// stop the video when the function is called
+function stopVideo() {
+    video.currentTime=0;
+    video.pause();
+}
+
+
+// event listener
+video.addEventListener("click", toggleVideoStatus);
+video.addEventListener("pause",updatePlayIcon);
+video.addEventListener("play", updatePlayIcon);
+video.addEventListener("timeupdate",updateProgress);
+
+play.addEventListener("click", toggleVideoStatus);
+
+stop.addEventListener("click", stopVideo);
+
+progress.addEventListener("change",setVideoProgress);
